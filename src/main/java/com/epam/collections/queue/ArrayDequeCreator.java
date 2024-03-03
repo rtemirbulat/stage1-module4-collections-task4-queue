0f9dc51798244a45fad6b80ecb0e95d88817af70
rtemirbulat <equalityisnotequality@gmail.com>
2024-03-03 23:29:20 +0500

    ..

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,26 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(firstQueue.poll());
+        deque.add(firstQueue.poll());
+        deque.add(secondQueue.poll());
+        deque.add(secondQueue.poll());
+
+        if(!secondQueue.isEmpty() && !firstQueue.isEmpty()){
+            while(!secondQueue.isEmpty() && !firstQueue.isEmpty()){
+                //FirstPlayerMove:
+                firstQueue.add(deque.pollLast());
+                //FirstPlayerMove:
+                deque.add(firstQueue.poll());
+                deque.add(firstQueue.poll());
+                //SecondPlayerMove:
+                secondQueue.add(deque.pollLast());
+                //SecondPlayerMove:
+                deque.add(secondQueue.poll());
+                deque.add(secondQueue.poll());
+            }
+        }
+        return deque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,25 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+
+        List<Integer> dishNumbers = new ArrayList<>();
+        while(!queue.isEmpty()){
+            for (int i = 0; i < everyDishNumberToEat - 1; i++) {
+                queue.add(queue.poll());
+            }
+            dishNumbers.add(queue.poll());
+        }
+
+        return dishNumbers;
     }
 }
